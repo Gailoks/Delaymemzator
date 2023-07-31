e6b7d75aa41fe706d46e5848546797f3a4e5f2ff
LloydLion <lloydlion2211@gmail.com>
2023-08-01 00:13:31 +0300

    Logging and something more

--- abstractions.py
+++ abstractions.py
@@ -1,62 +1,86 @@
 from __future__ import annotations
 import models
 from abc import ABC, abstractmethod
-from asyncio import coroutine
 import models
+from typing import Awaitable
+from logtools import *
 
 class MemeSource(ABC):
     @abstractmethod
     def set_router(self, router: MemeRouter) -> None:
-        pass
+        raise NotImplementedError()
 
 class MemeIntake(ABC):
     @abstractmethod
-    def upload_meme(self, sender_id: int, meme: models.Meme):
-        pass
+    def upload_meme(self, sender_id: int, meme: models.Meme) -> Awaitable[None]:
+        raise NotImplementedError()
 
     @abstractmethod
-    def validate_upload(self, sender_id: int, meme: models.Meme):
-        pass
+    def validate_user(self, sender_id: int) -> Awaitable[str | None]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def validate_upload(self, sender_id: int, meme: models.Meme) -> Awaitable[str | None]:
+        raise NotImplementedError()
 
 
 class MemeRouter:
-    def __init__(self, sources: list[MemeSource], intakes: dict[str, MemeIntake]):
+    def __init__(self, sources: list[MemeSource], intakes: dict[str, MemeIntake], logger: Logger):
         self.__sources = sources
         self.__intakes = intakes
+        self.__logger = logger
         pass
 
     def initialize(self):
         for source in self.__sources:
             source.set_router(self)
 
-    async def route_meme(self, sender_id: int, addresses: list[str], meme: models.Meme):
+    async def route_memes(self, sender_id: int, addresses: list[str], memes: list[models.Meme]) -> Awaitable[str]:
+        routing_result = await self.__route_meme_internal(sender_id, addresses, memes)
+        self.__logger.log(LogLevel.INFORMATION, LogEvent(11, "MemeRouted"),
+            "Memes from {SenderId} that contains {MemeCount} memes routed to {Addresses} with result {RoutingResult}",
+            [sender_id, len(memes), addresses, routing_result])
+
+
+    async def __route_meme_internal(self, sender_id: int, addresses: list[str], memes: list[models.Meme]) -> Awaitable[str]:
+        try:
+            for address in addresses:
+                if address not in self.__intakes:
+                    return f"Unknown address {address}"
+
+            targets = list(map(lambda x: (self.__intakes[x], x), addresses))
+
+            for intake in targets:
+                validation_result = await intake[0].validate_user(sender_id)
+                if validation_result:
+                    return f"You cannot send meme to {intake[1]}, reason: {validation_result}"
+
+            meme_codes = []
+
+            for meme_id, meme in enumerate(memes):
+                meme_id += 1 # start from 1
+
+                for intake in targets:
+                    validation_result = await intake[0].validate_upload(sender_id, meme)
+                    if validation_result:
+                        return f"Enable to send meme #{meme_id}, reason {validation_result}"
+
+                coroutines: list = [intake[0].upload_meme(sender_id, meme) for intake in targets]
+                errors: list[Exception] = []
+
+                for coroutine in coroutines:
+                    try:
+                        await coroutine
+                    except Exception as ex:
+                        errors.append(ex)
+
+                if errors:
+                    meme_codes.append("\n\t" + "\n\t".join(map(str, errors)))
+                else:
+                    meme_codes.append("Successful")
+
+
+            return '\n'.join(map(lambda x: f"[{x[0]}]: {x[1]}", enumerate(meme_codes)))
         
-        for address in addresses:
-            if address not in self.__intakes:
-                return f"Unknown address {address}"
-
-        targets = list(map(lambda x: (self.__intakes[x], x), addresses))
-
-        for intake in targets:
-            try:
-                await intake[0].validate_upload(sender_id, meme)
-            except Exception as ex:
-                return f"Enable to send to {intake[1]}, reason:\n{str(ex)}"
-
-        coroutines: list = []
-
-        for intake in targets:
-            coroutines.append(intake[0].upload_meme(sender_id, meme))
-
-        errors: list[Exception] = []
-
-        for coroutine in coroutines:
-            try:
-                await coroutine
-            except Exception as ex:
-                errors.append(ex)
-
-        if errors:
-            return "\n".join(map(str, errors))
-        else:
-            return "Successful"
+        except Exception as ex:
+            return f"Fatal exception in meme routing process, please contact with developers to fix it error. Error text:\n{ex}"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import models
 import abstractions
 import bots.vk.vkinteraction as vk
 import bots.vk.dbcontext as vkdb
@@ -6,10 +7,16 @@ import asyncio
 import dev
 import bots.telegram.telegraminteractions as tg
 import bots.telegram.dbcontext as tgdb
+import logtools
+import dateparser
 
 async def main():
     with open("config.json", 'r', encoding = "utf-8") as config:
         config = json.load(config)
+
+    logger_factory = logtools.LoggerFactory(logtools.LogLevel.TRACE)
+    logger_factory.add_logger(logtools.ConsoleLoggerProvider())
+
 
     vk_source_bot = vk.VkSourceBot(**config["vk-source"]["bot"])
     sources: list[abstractions.MemeSource] = [
@@ -20,18 +27,18 @@ async def main():
     await vkdb_context.connect(**config["vk-intake"]["database"]["sqlite"])
     vk_intake_bot = vk.VkIntakeBot(**config["vk-intake"]["bot"], db = vkdb_context)
 
-    tgdb_context = tgdb.SQLiteDBContext()
-    await tgdb_context.connect(**config["telegram-intake"]["database"]["sqlite"])
-    telegam_intake_bot = tg.TelegramIntakeBot(**config["telegram-intake"]["bot"], db = tgdb_context)
+    # tgdb_context = tgdb.SQLiteDBContext()
+    # await tgdb_context.connect(**config["telegram-intake"]["database"]["sqlite"])
+    # telegam_intake_bot = tg.TelegramIntakeBot(**config["telegram-intake"]["bot"], db = tgdb_context)
 
     intakes: dict[str, abstractions.MemeIntake] = {
         "вк": vk_intake_bot,
         "vk": vk_intake_bot,
-        "tg": telegam_intake_bot,
-        "тг": telegam_intake_bot
+        # "tg": telegam_intake_bot,
+        # "тг": telegam_intake_bot
     }
 
-    main_router = abstractions.MemeRouter(sources, intakes)
+    main_router = abstractions.MemeRouter(sources, intakes, logger_factory.create_logger("Router"))
     main_router.initialize()
 
     await vk_source_bot.run()
