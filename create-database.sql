5035952be0d9c340834c4b65f5d8e4c9367648bb
LloydLion <lloydlion2211@gmail.com>
2023-07-31 20:41:28 +0300

    Database SQL script

---
+++ create-database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TelegramGroups (
+    id       INTEGER NOT NULL,
+    group_id INTEGER NOT NULL
+);
+
+CREATE TABLE VkGroups (
+    id       INTEGER NOT NULL,
+    group_id INTEGER NOT NULL
+);
